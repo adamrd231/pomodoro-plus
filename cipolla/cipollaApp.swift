4ea93097d247cc43c0b94677d20d6f305b6d7e05
Adam Reed <adam.rdconcepts@gmail.com>
2022-06-28 10:44:54 -0600

    Create base file for new udpate to SwiftUI

---
+++ cipolla/cipollaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cipollaApp.swift
+//  cipolla
+//
+//  Created by Adam Reed on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct cipollaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
