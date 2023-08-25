8d83e008938889e9e3c1278108e4d71e05215c33
Adam Reed <adam.rdconcepts@gmail.com>
2023-08-25 12:12:59 -0400

    Start laying out options screen.

---
+++ cipolla/Views/CipollaTimerView.swift
@@ -0,0 +1,90 @@
+//
+//  CipollaTimerView.swift
+//  cipolla
+//
+//  Created by Adam Reed on 8/25/23.
+//
+
+import SwiftUI
+
+struct CipollaTimerView: View {
+    @EnvironmentObject var vm: HomeViewModel
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("Cipolla")
+                .font(.caption)
+            
+            Text(vm.pomodoroTimer.rounds == 0 ? "done" : "\(vm.pomodoroTimer.rounds.description) Rounds")
+                .font(.title3)
+                .fontWeight(.bold)
+            
+           
+            
+            switch vm.pomodoroTimer.timerOptionSelection {
+                case .pomodoro: TimerView(time: vm.pomodoroTimer.pomodoroTime)
+                case .shortBreak: TimerView(time: vm.pomodoroTimer.breakTime)
+            }
+               
+            Picker("", selection: $vm.pomodoroTimer.timerOptionSelection) {
+                ForEach(vm.pomodoroTimer.timerOptions, id: \.self) { option in
+                    Text(option.description)
+                }
+            }
+            .pickerStyle(.segmented)
+            List {
+                VStack(alignment: .center) {
+                    TextField("What's the focus?", text: $vm.newTask)
+                    HStack {
+                        Button("Add task") {
+                            print("Add task to list for current pomodoro")
+                            vm.addItemToTaskList()
+                            
+                        }
+                        .disabled(vm.pomodoroTimer.isTimerRunning == .isRunning)
+                        Spacer()
+                    }
+                   
+                }
+                ForEach($vm.taskList, id: \.self, editActions: .delete) { $task in
+                    HStack {
+                        Toggle("", isOn: $task.isComplete)
+                            .toggleStyle(CheckboxToggleStyle())
+                            .fixedSize()
+                        Text(task.name)
+                    }
+                }
+                Button("Clear list") {
+                    vm.taskList = []
+                }
+                .disabled(vm.pomodoroTimer.isTimerRunning == .isRunning)
+                
+            }
+            .frame(width: UIScreen.main.bounds.width)
+            .frame(maxWidth: .infinity)
+            
+            Button {
+                print("Something")
+                switch vm.pomodoroTimer.isTimerRunning {
+                    case .isPaused: vm.startTimer()
+                    case .isRunning: vm.pauseTimer()
+                    case .isDone: vm.totalReset()
+                }
+            } label: {
+                switch vm.pomodoroTimer.isTimerRunning {
+                    case .isPaused: Text("Timer to focus")
+                    case .isRunning: Text("Pause")
+                    case .isDone: Text("Reset")
+                }
+            }
+          
+        }
+        .padding()
+    }
+}
+
+struct CipollaTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CipollaTimerView()
+    }
+}
--- cipolla/Views/HomeView.swift
+++ cipolla/Views/HomeView.swift
@@ -17,8 +17,34 @@ struct HomeView: View {
     
     var body: some View {
         // Status showing what step user is on
-        CipollaTimerView()
-            .environmentObject(vm)
+        TabView {
+            CipollaTimerView()
+                .environmentObject(vm)
+                .tabItem {
+                    VStack {
+                        Text("Home")
+                        Image(systemName: "house")
+                    }
+                }
+            
+            VStack {
+                Text("Options")
+                List {
+                    HStack {
+                        Text("Rounds")
+                        Spacer()
+                        Stepper("\(vm.pomodoroTimer.rounds)", value: $vm.pomodoroTimer.rounds, in: 0...10)
+                    }
+                }
+            }
+            .tabItem {
+                VStack {
+                    Text("Options")
+                    Image(systemName: "option")
+                }
+            }
+        }
+      
     }
 }
 
